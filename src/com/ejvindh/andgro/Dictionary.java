51a87accbdaed7e4ec342218f0c0a2a69ad3432b
ejvindh <ejvindh@gmail.com>
2013-07-04 00:22:58 +0200

    Første commit

---
+++ src/com/ejvindh/andgro/Dictionary.java
@@ -0,0 +1,164 @@
+package com.ejvindh.andgro;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+public class Dictionary {
+	
+	public static String[][] lookup(String search_string, String shortname, 
+			String gddfile, String datfile, String dict_dir, 
+			int doubflag) throws IOException, java.sql.SQLException, ClassNotFoundException {
+		// Hovedopgaven for denne metode er at finde ud af hvilke tabeller der skal kigges i i Gdd-databasen
+		// Til slut kaldes metoden getEntries for at finde vektorerne i gdd-databasen for de indtastede søgeord
+		// Og getRawEntryText bruges til at udtrække selve opslagene i dat-databasen
+		
+		String[][] results = new String[2][3];
+		ArrayList<String> tables = new ArrayList<String>();
+		tables.add("lookup");
+		tables.add("reverse");
+		tables.add("collocation_lookup");
+		File sql_file = new File(dict_dir + "/" + gddfile ); 
+		SQLiteDatabase sql_conn = SQLiteDatabase.openDatabase(
+				sql_file.getAbsolutePath(), null, SQLiteDatabase.OPEN_READONLY);
+		RandomAccessFile dat_conn = new RandomAccessFile(dict_dir+"/"+datfile, "r");
+		
+		if (doubflag > 1) { // hvis ordbogen er tovejs
+			for (int d_=0; d_<2; d_++) {
+				boolean d = d_==0;
+				int t_count = 0;
+				for (String table: tables) {
+					if (table.equals("reverse") || table.equals("collocation_lookup")) {
+						d=!d;
+					}
+					//søg ikke i reverse, hvis databasen ikke indeholder reverse
+					if ((table.equals("reverse") && (doubflag==0 || doubflag==2))) {
+						results[d_][t_count]="";
+					} else {
+						ArrayList<int[]> entries=getEntries(sql_conn, search_string, table, d);
+						String entrytext=getRawEntryText(dat_conn, entries);
+						results[d_][t_count]=entrytext;
+					}
+					t_count++;
+				}
+			}
+		} else { // hvis ordbogen er envejs
+			boolean d = true;
+			int t_count = 0;
+			for (String table: tables) {
+				//søg ikke i reverse, hvis databasen ikke indeholder reverse
+				if ((table.equals("reverse") && (doubflag==0 || doubflag==2))) {
+					results[0][t_count]="";
+				} else {
+					ArrayList<int[]> entries=getEntries(sql_conn, search_string, table, d);
+					String entrytext=getRawEntryText(dat_conn, entries);
+					results[0][t_count]=entrytext;
+				}
+				t_count++;
+			}
+		}
+		sql_conn.close();
+		dat_conn.close();
+		for (int x=0; x<=((doubflag^1)&2)/2; x++) { //ligningen giver 1, hvis envejs; ellers 2
+			if (results[x][0].equals("")) {
+				StringBuffer henv = new StringBuffer();
+				henv.append("Opslaget er fundet i");
+				if (results[x][1].compareTo("")!=0) {
+					henv.append(" 'omvendt søgning'");
+					if (results[x][2].compareTo("")!=0) henv.append(" &");
+				}
+				if (results[x][2].compareTo("")!=0) henv.append(" Eksempelsætninger");
+				if (henv.toString().equals("Opslaget er fundet i")) {
+					results[x][0]="Opslag ikke fundet";
+				} else {
+					results[x][0]=henv.toString();
+				}
+			}
+		}
+		return results;
+		/* results[0][0]=fromdanish_lookup
+		 * results[0][1]=fromdanish_reverse
+		 * results[0][2]=fromdanish_collocation_lookup (eksempelsætninger)
+		 * results[1][0]=todanish_lookup
+		 * results[1][1]=todanish_reverse
+		 * results[1][2]=todanish_collocation_lookup (eksempelsætninger)
+		 */
+	}
+	
+	public static ArrayList<int[]> getEntries(SQLiteDatabase sql_conn, String search_string, String table, boolean d) 
+			throws SQLException, java.sql.SQLException {
+		// Denne metode bruges til at finde vektorerne i gdd-databasen for de indtastede søgeord
+
+		String[] src_terms = search_string.split(" ");
+        ArrayList<String> src_terms_list = new ArrayList<String>();
+		for(int i=0; i< src_terms.length; i++){
+        	src_terms_list.add(src_terms[i]);
+        }
+        int fromDanish=0;
+        if (d) fromDanish=1; else fromDanish=2;
+        boolean first_term = true;
+		ArrayList<Integer> entry_ids = new ArrayList<Integer>();
+		ArrayList<Integer> term_entry_ids = new ArrayList<Integer>();
+		for (String term : src_terms_list) {
+			String selectQuery = "select * from " + table + fromDanish + " where word_ like '" + term + "'";
+			Cursor rows = sql_conn.rawQuery(selectQuery, null);
+			term_entry_ids.clear();
+			if (rows.moveToFirst()) {
+				do {
+					term_entry_ids.add(rows.getInt(0));
+	            } while (rows.moveToNext());
+	        }
+			if (first_term) {
+				first_term = false;
+	    		entry_ids.addAll(term_entry_ids);
+	    	} else {
+				ArrayList<Integer> a1 = new ArrayList<Integer>(entry_ids);
+	    		entry_ids.clear();
+	    		for (int a : term_entry_ids) {
+	    			if (a1.contains(a)) entry_ids.add(a);
+	    		}
+	    	}
+			rows.close();
+	    }
+	    ArrayList<int[]> entries = new ArrayList<int[]>();
+		for (int entry_id: entry_ids) {
+			String selectQuery = "select * from entries" + fromDanish + " where id_ = " + entry_id;
+			Cursor rows = sql_conn.rawQuery(selectQuery, null);
+			if (rows.moveToFirst()) {
+	            do {
+		    		entries.add(new int [] {entry_id, rows.getInt(3), rows.getInt(4)});
+	            } while (rows.moveToNext());
+	        }
+	    }
+		return entries;
+	}
+
+	public static String getRawEntryText(RandomAccessFile dat_file, ArrayList<int[]> entries) throws IOException {
+		ArrayList<String> raw_entries = new ArrayList<String>();
+		// Denne metode bruges til at udtrække selve opslagene i dat-databasen
+
+		for (int i=0; i<entries.size(); i++) {
+			int entry_id=entries.get(i)[0];
+			int offset=entries.get(i)[1];
+			int nbyte=entries.get(i)[2];
+			dat_file.seek(offset);
+			ArrayList<Byte> data = new ArrayList<Byte>();
+			for (int n=0; n<nbyte; n++) {
+			    data.add(n, dat_file.readByte());
+			}
+			String raw_entry = Groparser.parse_entry(data, entry_id);
+			String filtered_entry = Groparser.filter_entry(raw_entry);
+			raw_entries.add(filtered_entry);
+		}
+		StringBuffer raw_entries_collect_to_string = new StringBuffer();
+		for (String r:raw_entries) {
+			raw_entries_collect_to_string.append(r);
+		}
+		return raw_entries_collect_to_string.toString();
+	}
+}
---
+++ src/com/ejvindh/andgro/MainActivity.java
@@ -0,0 +1,382 @@
+package com.ejvindh.andgro;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.os.Environment;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.WindowManager;
+import android.webkit.WebView;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.ToggleButton;
+
+public class MainActivity extends Activity implements OnClickListener {
+
+	public final static String EXTRA_MESSAGE = "com.ejvindh.andgro.MESSAGE";
+	static final int CREATE_REQUEST_CODE = 0;
+	private EditText src_txt;
+	private Button src;
+	private CheckBox todan;
+	private ImageView todan_im;
+	private CheckBox fromdan;
+	private ImageView fromdan_im;
+	private ToggleButton eks;
+	private ToggleButton rev;
+	private Spinner dictnr;
+	private WebView show_search_result;
+	private String dict_dir;
+	private ArrayAdapter<String> adapter;
+	private ArrayList<String[]> dictionaries;
+	private String[][] results = new String[2][3];
+			
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		for (int x=0; x<2; x++) {
+			for (int y=0; y<3; y++) {
+				results[x][y]="";
+			}
+		}
+		src = (Button) findViewById(R.id.src_button);
+		src.setOnClickListener(this);
+		todan = (CheckBox) findViewById(R.id.checkBox_todan);
+		todan.setOnClickListener(this);
+		todan_im = (ImageView) findViewById(R.id.imageView_todan);
+		fromdan = (CheckBox) findViewById(R.id.checkBox_fromdan);
+		fromdan.setOnClickListener(this);
+		fromdan_im = (ImageView) findViewById(R.id.imageView_fromdan);
+		eks = (ToggleButton) findViewById(R.id.toggleButton_eks);
+		eks.setOnClickListener(this);
+		rev = (ToggleButton) findViewById(R.id.toggleButton_rev);
+		rev.setOnClickListener(this);
+		show_search_result = (WebView) findViewById(R.id.webView_src_result);
+		show_search_result.getSettings().setSupportZoom(true);
+		show_search_result.getSettings().setBuiltInZoomControls(true);
+	    
+		
+		SharedPreferences settings = getSharedPreferences("AndGro_settings", 0);
+		
+		//Hent gemte settings, og hvis de ikke findes, så loade default
+		dict_dir = settings.getString("dict_dir_settings", Environment.
+				getExternalStorageDirectory().toString());
+		int lang_sel = settings.getInt("dictnr_settings", 0);
+		fromdan.setChecked(settings.getBoolean("fromdan_settings", true));
+		todan.setChecked(settings.getBoolean("todan_settings", true));
+		eks.setChecked(settings.getBoolean("eks_settings", true));
+		rev.setChecked(settings.getBoolean("rev_settings", true));
+		dictionaries = Get_books.parse_dictionaries(this, dict_dir);
+		createSpinner(dictionaries, lang_sel);
+
+		//Når der vælges nyt sprog
+		dictnr.setOnItemSelectedListener(new OnItemSelectedListener() {
+		    @Override
+		    public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+				//ved skift af sprog
+		    	set_views(dictnr.getSelectedItemPosition());
+		    	for (int x=0; x<2; x++) {
+	        		for (int y=0; y<3; y++) {
+	        			results[x][y]="";
+	        		}
+	        	}
+			show_results();
+		    }
+		    @Override
+		    public void onNothingSelected(AdapterView<?> parentView) {
+		    }
+		});
+
+		src_txt = (EditText) findViewById(R.id.src_txt_widget);
+		//Hold øje med om der tastes "afslut" på tastaturet
+		src_txt.setOnKeyListener(new View.OnKeyListener() {
+			@Override
+			public boolean onKey(View v, int keyCode, KeyEvent event) {
+				// If the event is a key-down event on the "enter" button
+		        if ((event.getAction() == KeyEvent.ACTION_DOWN) &&
+		            (keyCode == KeyEvent.KEYCODE_ENTER)) {
+		          // Perform action on key press
+		          if (!dictionaries.isEmpty()) {
+		        	  get_results();
+		        	  show_results();
+		          }
+		          return true;
+		        }
+		        return false;
+			}
+		});
+		set_views(dictnr.getSelectedItemPosition());
+	}
+	
+	private void createSpinner(ArrayList<String[]> dictionaries2, int lang_sel) {
+		//Fyld indhold i Comboboxen
+		String[] diction = new String[dictionaries.size()];
+		if (!dictionaries.isEmpty()) {
+			for (int i = 0; i < dictionaries.size(); i++) {
+				diction[i]=dictionaries.get(i)[5];
+			}
+		}
+		adapter = new ArrayAdapter<String>(this, R.layout.spinner_item, diction);
+		
+		//ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.spinner_item,list);
+		
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		dictnr = (Spinner) findViewById(R.id.select_lang);
+		dictnr.setAdapter(adapter);
+		if (lang_sel >= dictionaries.size()) {
+			lang_sel = 0;
+		}
+		dictnr.setSelection(lang_sel);
+	}
+
+	private void set_views(int selectedItemPosition) {
+		//Hvilke Checkboxes skal vises. Hvad skal greyes, osv.
+		if (!dictionaries.isEmpty()) {
+			if (!fromdan.isEnabled()) {
+				src_txt.setEnabled(true);
+				dictnr.setEnabled(true);
+				src.setEnabled(true);
+				fromdan.setEnabled(true);
+				todan.setEnabled(true);
+				eks.setEnabled(true);
+				rev.setEnabled(true);
+				show_search_result.loadDataWithBaseURL("fake-url", "<html></html>", "text/html", "UTF-8", null);
+				show_search_result.loadUrl("about:blank");
+			}
+			int lang_sel = selectedItemPosition;
+			int doubflag = Integer.parseInt(dictionaries.get(lang_sel)[4]);
+			String flag_name_fromdan = "da"+dictionaries.get(lang_sel)[0];
+			String flag_name_todan = dictionaries.get(lang_sel)[0]+"da";
+			Resources res = getResources();
+			int resourceId = res.getIdentifier(
+					flag_name_fromdan, "drawable", getPackageName() );
+			fromdan_im.setImageResource( resourceId );
+			if (doubflag < 2) {
+				todan.setVisibility(4);
+				todan_im.setVisibility(4);
+			} else {
+				todan.setVisibility(0);
+				todan_im.setVisibility(0);
+				resourceId = res.getIdentifier(
+						flag_name_todan, "drawable", getPackageName() );
+				todan_im.setImageResource( resourceId );
+			}
+			if ((doubflag&1)==0) {
+				rev.setVisibility(4);
+			} else {
+				rev.setVisibility(0);
+			}
+		} else {
+			src_txt.setEnabled(false);
+			dictnr.setEnabled(false);
+			src.setEnabled(false);
+			fromdan.setEnabled(false);
+			todan.setEnabled(false);
+			eks.setEnabled(false);
+			rev.setEnabled(false);
+			show_search_result.loadData("Der blev ikke fundet nogen ordbøger." +
+					"<br><br><small><small>Ordbøgerne skal lægges ind i en mappe på enheden. Har du " +
+					"allerede gjort dette, skal du klikke på menu-knappen, og søge efter " +
+					"dem på enheden.<br><br>Denne ordbogslæser er kun kompatibel med " +
+					"Gyldendals ordbøger i download-udgaven. Disse købes på " +
+					"http://ordbog.gyldendal.dk<br><br>Når du har købt programmet, skal du " +
+					"installere det (på en PC eller Mac) og så kan du hente database-filerne " +
+					"inde i '%Program Files%Gyldendal/Røde Ordbøger/data'. Hver ordbog består " +
+					"af en *.gdd og en *.dat-fil.<br><br>Eks: For at få adgang til den tyske " +
+					"ordbog skal du således uploade filerne Tyskordbog.gdd og Tyskordbog.dat</small></small>"
+					, "text/html; charset=utf-8", "UTF-8");
+		}
+	}
+	private void get_results() {
+		//Så skal der søges i ordbogsdatabaserne
+		for (int x=0; x<2; x++) {
+    		for (int y=0; y<3; y++) {
+    			results[x][y]="";
+    		}
+    	}
+		try {
+			results = Dictionary.lookup(src_txt.getText().toString(), dictionaries.get(dictnr.getSelectedItemPosition())[0], 
+					dictionaries.get(dictnr.getSelectedItemPosition())[2], dictionaries.get(dictnr.getSelectedItemPosition())[3], 
+					dict_dir, Integer.parseInt(dictionaries.get(dictnr.getSelectedItemPosition())[4]));
+		} catch (NumberFormatException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void show_results() {
+		//Visning af opslaget i webview
+		//TODO: hvis man drejer telefonen i den anden retning, så nulstilles webview
+		int doub = Integer.parseInt(dictionaries.get(dictnr.getSelectedItemPosition())[4]);
+		boolean fromDanish = fromdan.isChecked();
+		boolean toDanish = todan.isChecked();
+		boolean eks_flag = eks.isChecked();
+		boolean rev_flag = rev.isChecked();
+		String lang_longname = dictionaries.get(dictnr.getSelectedItemPosition())[1];
+		
+		ArrayList<String> opsamling = new ArrayList<String>();
+		opsamling.add("<!DOCTYPE html PUBLIC '-//W3C//DTD XHTML 1.0 Transitional//EN' " +
+				"'http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd'><html xmlns=" +
+				"'http://www.w3.org/1999/xhtml' xml:lang='da' lang='da'>" +
+				"<head><title>Opslag</title><meta http-equiv='Content-Type' content='text/html; charset=utf-8' /></head>" +
+				"<body><small><small>");
+		if (results!=null) {
+			if (doub==0 || doub==2) rev_flag=false;
+			if (fromDanish) {
+				if (results[0][0].compareTo("")!=0) {
+					if (doub>1) {
+						opsamling.add("<h1>Dansk - " + lang_longname + "<br/><br/></h1>");
+					} else {
+						opsamling.add("<h1>" + lang_longname + "<br/><br/></h1>");
+					}
+					opsamling.add(results[0][0]);
+					opsamling.add("<br/>");
+				}
+				if(rev_flag && results[0][1].compareTo("")!=0) {
+					opsamling.add("<h2>Forekomster i 'omvendt søgning'<br/></h2>");
+					opsamling.add(results[0][1]);
+					opsamling.add("<br/>");
+				}
+				if(eks_flag && results[0][2].compareTo("")!=0) {
+					opsamling.add("<h2>Eksempelsætninger<br/></h2>");
+					opsamling.add(results[0][2]);
+					opsamling.add("<br/><br/>");
+				}
+			}
+			if (toDanish && doub>1) {
+				if (results[1][0].compareTo("")!=0) {
+					opsamling.add("<h1>" + lang_longname + "-Dansk<br/><br/></h1>");
+					opsamling.add(results[1][0]);
+					opsamling.add("<br/>");
+				}
+				if(rev_flag && results[1][1].compareTo("")!=0) {
+					opsamling.add("<h2>Forekomster i 'omvendt søgning'<br/></h2>");
+					opsamling.add(results[1][1]);
+					opsamling.add("<br/>");
+				}
+				if(eks_flag && results[1][2].compareTo("")!=0) {
+					opsamling.add("<h2>Eksempelsætninger<br/></h2>");
+					opsamling.add(results[1][2]);
+				}
+			}
+		}
+		opsamling.add("</small></small></body></html>");
+		StringBuffer t_ = new StringBuffer();
+		t_.setLength(0);
+		for (String t:opsamling) {
+			t_.append(t);
+		}
+		show_search_result.loadDataWithBaseURL("fake-url", "<html></html>", "text/html", "UTF-8", null);
+		show_search_result.loadData(t_.toString(), "text/html; charset=utf-8", "UTF-8");
+	}
+
+	
+	
+	@Override
+	protected void onStop() {
+		super.onStop();
+		//Gem settings
+		//TODO: skulle dette mon også ind i en onPause??
+		SharedPreferences settings = getSharedPreferences("AndGro_settings", 0);
+		SharedPreferences.Editor editor = settings.edit();
+		editor.putInt("dictnr_settings", dictnr.getSelectedItemPosition());
+		editor.putBoolean("todan_settings", todan.isChecked());
+		editor.putBoolean("fromdan_settings", fromdan.isChecked());
+		editor.putBoolean("eks_settings", eks.isChecked());
+		editor.putBoolean("rev_settings", rev.isChecked());
+		editor.putString("dict_dir_settings", dict_dir);
+		editor.commit();
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		//Danne settings-menuen
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.mymenu, menu);
+		return true;
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		if (item.getItemId()== R.id.find_dict) {
+			//Finde mappe med ordbogsfiler
+			Intent intent = new Intent(this, BrowseFiles.class);
+			intent.putExtra(EXTRA_MESSAGE, dict_dir);
+			startActivityForResult(intent, CREATE_REQUEST_CODE);
+		}
+		if (item.getItemId()== R.id.stay_on_setting) {
+			//Forhindre at apparatet går i standby
+			item.setChecked(!item.isChecked());
+			if (item.isChecked()) {
+				new AlertDialog.Builder(this)
+				 .setIcon(R.drawable.ic_launcher)
+				 .setTitle("Kan være hård ved batteriet!")
+				 .setPositiveButton("OK", 
+						 new DialogInterface.OnClickListener() {
+					 @Override
+					 
+					 public void onClick(DialogInterface dialog, int which) {
+						 // Auto-generated method stub
+					 }
+					 }).show();
+				getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+			} else {
+				getWindow().clearFlags(android.view.WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+			}
+		}
+		return super.onOptionsItemSelected(item);
+	}
+	
+	
+	
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		//Her returneres biblioteket for ordbøgerne, hvis brugeren har været i "settings"-aktiviteten
+		if (requestCode == CREATE_REQUEST_CODE) {
+		      if (resultCode == RESULT_OK) {
+		    	dict_dir = data.getStringExtra("RESULT_STRING");
+		        dictionaries = Get_books.parse_dictionaries(this, dict_dir);
+		        fromdan.setChecked(true);
+		        todan.setChecked(true);
+		        eks.setChecked(true);
+		        rev.setChecked(true);
+		        dictnr.setSelection(0);
+		        createSpinner(dictionaries, dictnr.getSelectedItemPosition());
+		        set_views(dictnr.getSelectedItemPosition());
+		      }
+		   }
+		}
+	@Override
+	public void onClick(View v) {
+		//Hvis søgeknappen trykkes
+		if (v.getId() == R.id.src_button) {
+			if (!dictionaries.isEmpty()) {
+	        	  get_results();
+	        }
+		}
+		show_results();
+	}
+}
